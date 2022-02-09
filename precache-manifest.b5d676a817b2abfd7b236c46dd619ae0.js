0c2011c0609db9e645617dd78ee3d9954b4d559b
smcreater <master@smcreater.com>
2022-02-09 21:09:50 +0900

    Updates

---
+++ precache-manifest.b5d676a817b2abfd7b236c46dd619ae0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c92109498ae676d8fb25b1a5ab2c44be",
+    "url": "/habit/index.html"
+  },
+  {
+    "revision": "4781483ace25dfd3d5a4",
+    "url": "/habit/static/css/main.9b00fc92.chunk.css"
+  },
+  {
+    "revision": "c32d30b6973e066851fe",
+    "url": "/habit/static/js/2.98df370e.chunk.js"
+  },
+  {
+    "revision": "38d5cc1cb1001b9cc38b06e2da080c18",
+    "url": "/habit/static/js/2.98df370e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4781483ace25dfd3d5a4",
+    "url": "/habit/static/js/main.4af84f10.chunk.js"
+  },
+  {
+    "revision": "1dab6d8492545bc5011c",
+    "url": "/habit/static/js/runtime-main.8e7e9b5b.js"
+  }
+]);
